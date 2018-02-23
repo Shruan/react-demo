e9ceff948e4709331bba1bcfb6a96b599861e3c9
Shruan <qiushiyuan1994@qq.com>
2018-02-23 17:47:08 +0800

    state

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { render } from 'react-dom'
+// import { render } from 'react-dom'
 import logo from './logo.svg';
 
 class Header extends Component {
--- src/containers/Hello/index.jsx
+++ src/containers/Hello/index.jsx
@@ -3,6 +3,12 @@ import React, { Component } from 'react'
 import Test from './subPage/test'
 
 class Hello extends Component {
+  constructor (props, context) {
+    super (props, context)
+    this.state = {
+      now: Date.now()
+    }
+  }
   render () {
     const aaa = 'aaa111'
     return (
@@ -10,8 +16,15 @@ class Hello extends Component {
         1111
         <Test title="aaa"/>
         {aaa}
+        <p>当前时间戳：{this.state.now}</p>
+        <button onClick={this.updateDate.bind(this)}>update</button>
       </div>
     );
+  }
+  updateDate () {
+    this.setState({
+      now: Date.now()
+    })
   }
 }
 
