657450adc5904ff8ff32593fd7798cd837ecc369
Shruan <qiushiyuan1994@qq.com>
2018-02-24 16:20:47 +0800

    子组件接收props接收数据

--- src/containers/Hello/subPage/test.jsx
+++ src/containers/Hello/subPage/test.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class test extends Component {
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      testKey: '时间'
+    }
+  }
   render () {
     return (
-      <div>test{this.props.title}</div>
+      <div>test{this.props.title}, {this.state.testKey}</div>
     )
   }
+}
+
+test.propTypes = {
+  title: PropTypes.string.isRequired
 }
 
 export default test
