57a5a9e04ec55386bed7969d55f739b2de75801b
Shruan <qiushiyuan1994@qq.com>
2018-02-24 16:20:19 +0800

    onClick事件this指向绑定

--- src/containers/Hello/index.jsx
+++ src/containers/Hello/index.jsx
@@ -4,28 +4,30 @@ import Test from './subPage/test'
 
 class Hello extends Component {
   constructor (props, context) {
-    super (props, context)
+    super(props, context)
     this.state = {
       now: Date.now()
     }
-  }
-  render () {
-    const aaa = 'aaa111'
-    return (
-      <div>
-        1111
-        <Test title="aaa"/>
-        {aaa}
-        <p>当前时间戳：{this.state.now}</p>
-        <button onClick={this.updateDate.bind(this)}>update</button>
-      </div>
-    );
+    // 方法的this指向绑定
+    this.updateDate = this.updateDate.bind(this)
   }
   updateDate () {
     this.setState({
       now: Date.now()
     })
   }
+  render () {
+    const aaa = 'aaa111'
+    return (
+      <div>
+        1111
+        <Test title="aaa" />
+        {aaa}
+        <p>当前时间戳：{this.state.now}</p>
+        <button onClick={this.updateDate}>update</button>
+      </div>
+    )
+  }
 }
 
-export default Hello;
+export default Hello
