a1c0a217a649edd1a4061b2c63d3e4966030c9cb
Shruan <qiushiyuan1994@qq.com>
2018-02-27 02:40:19 +0800

    eslint改进

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,11 +24,11 @@ module.exports = {
   // 使用第三方airbnb开发配置合集
   "extends": "airbnb",
   // eslint-config-airbnb包括了以下3个插件
-  // "plugins": [
-  //   "react",
-  //   "jsx-a11y",
-  //   "import"
-  // ],
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
   // 定义自己的规则(仿standard)
   "rules": {
     "no-trailing-spaces": ["error", {"skipBlankLines": false}], // 不允许过多空行
@@ -37,5 +37,27 @@ module.exports = {
     "comma-dangle": ["error", "never"], // 要求或禁止末尾逗号：不允许逗号
     // "linebreak-style": ["error", "windows"], // window环境
     "react/forbid-prop-types": [0], // propTypes检查允许使用object array等
+    "arrow-parens": [0],
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "linebreak-style": ["error", "unix"],
+    "no-empty": 0,
+    "no-unused-vars": 0,
+    "no-console": 0,
+    "no-const-assign": 2,
+    "no-dupe-class-members": 2,
+    "no-duplicate-case": 2,
+    "no-extra-parens": [2, "functions"],
+    "no-self-compare": 2,
+    "accessor-pairs": 2,
+    "comma-spacing": [2, {"before": false, "after": true}],
+    "constructor-super": 2,
+    "new-cap": [2, {"newIsCap": true, "capIsNew": false}],
+    "new-parens": 2,
+    "no-array-constructor": 2,
+    "no-class-assign": 2,
+    "no-cond-assign": 2,
+    "class-methods-use-this": 0,
+    "react/jsx-no-bind": 0
   }
 }
