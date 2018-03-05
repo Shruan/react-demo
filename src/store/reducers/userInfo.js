e7d9cfc9481f21b63fe7a1c50daadff024ca6998
Shruan <qiushiyuan1994@qq.com>
2018-03-05 23:51:42 +0800

    redux优化

--- src/store/reducers/userInfo.js
+++ src/store/reducers/userInfo.js
@@ -5,20 +5,17 @@ const initialState = {
 }
 
 export default function userInfo (state = initialState, action) {
+  if (action.data) {
+    for (const obj in state) {
+      if (!action.data.hasOwnProperty(obj)) {
+        action.data[obj] = state[obj]
+      }
+    }
+  }
   switch (action.type) {
     case 'USERINFO_LOGIN':
-      for (const obj in state) {
-        if (!action.data.hasOwnProperty(obj)) {
-          action.data[obj] = state[obj]
-        }
-      }
       return action.data
     case 'UPDATE_CITYNAME':
-      for (const obj in state) {
-        if (!action.data.hasOwnProperty(obj)) {
-          action.data[obj] = state[obj]
-        }
-      }
       return action.data
     default:
       return state
