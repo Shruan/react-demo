e226dc022618d6fa258facffdf97c2184f21a2c8
Shruan <qiushiyuan1994@qq.com>
2018-03-08 01:11:03 +0800

    引入antd UI

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+//
 import Router from './router/index.jsx'
+
+// react-redux
 import { Provider } from 'react-redux'
 import store from './store'
+
+// 配置文件
 import registerServiceWorker from './registerServiceWorker';
+import './assets/css/common.less'
+import './assets/css/font.css'
+
+// ant-design
+import 'antd-mobile/dist/antd-mobile.css';  // or 'antd-mobile/dist/antd-mobile.less'
 
 const configureStore = store()
 
